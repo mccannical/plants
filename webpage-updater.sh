7e3001fe82561f9bc4260be8789309b73b981c3e
Jesse <jesse@mccannical.com>
2021-01-21 10:26:50 -0500

    Update webpage-updater.sh

--- webpage-updater.sh
+++ webpage-updater.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 sleep_time=30
-
-wall -n Gather secrets.
 source /home/pi/plants/secrets
-
-wall -n move home
 cd /home/pi/plants || end
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting webpage-updater"}' ${slack}
 
-
-wall -n updating index.html...
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Updating index.html: http://picam-garden-jesse.s3-website.us-east-2.amazonaws.com/"}' ${slack}
 s3cmd put --acl-public index.html s3://picam-garden-jesse/index.html
 
-wall -n website http://picam-garden-jesse.s3-website.us-east-2.amazonaws.com/
 mkdir timelapse 
 counter=1000
 while true; do
-        wall -n Picture ${counter}. 
         raspistill -w 1024 -h 768 -o raw.jpg
         cp raw.jpg "timelapse/image-${counter}.jpg"
         convert raw.jpg -pointsize 32 -fill white -annotate +220+160 "$(date +"%a %r")"  plants.jpg
         s3cmd put --acl-public plants.jpg s3://picam-garden-jesse/img/plants.jpg
         rm raw.jpg plants.jpg
-        wall -n Sleeping ${sleep_time}s
         sleep ${sleep_time}
         ((counter=counter+1))
 done
