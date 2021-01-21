fafe9c230ebbd0f1b6f1389bf969b4bf57d65375
Jesse <jesse@mccannical.com>
2021-01-20 22:43:55 -0500

    Update webpage-updater.sh

--- webpage-updater.sh
+++ webpage-updater.sh
@@ -9,8 +9,7 @@ cd /home/pi/plants || end
 
 
 wall -n updating index.html...
-s3cmd put index.html s3://picam-garden-jesse/index.html
-s3cmd setacl --acl-public --recursive s3://picam-garden-jesse/index.html
+s3cmd put --acl-public index.html s3://picam-garden-jesse/index.html
 
 wall -n website http://picam-garden-jesse.s3-website.us-east-2.amazonaws.com/
 mkdir timelapse 
@@ -20,8 +19,7 @@ while true; do
         raspistill -w 1024 -h 768 -o raw.jpg
         cp raw.jpg "timelapse/image-${counter}.jpg"
         convert raw.jpg -pointsize 74 -fill white -annotate +100+100 "$(date +"%a %r")"  plants.jpg
-        s3cmd put plants.jpg s3://picam-garden-jesse/img/plants.jpg
-        s3cmd setacl --acl-public --recursive s3://picam-garden-jesse/img
+        s3cmd put --acl-public plants.jpg s3://picam-garden-jesse/img/plants.jpg
         rm raw.jpg plants.jpg
         wall -n Sleeping ${sleep_time}s
         sleep ${sleep_time}
