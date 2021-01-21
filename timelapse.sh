93100bf41792f32b6576a25c58adeee981b3198e
Jesse <jesse@mccannical.com>
2021-01-21 10:29:42 -0500

    Update timelapse.sh

--- timelapse.sh
+++ timelapse.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-wall -n Preparing Timelapse for the day
-wall -n Gather secrets.
 source /home/pi/plants/secrets
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting Processing of Timelapse for the day"}' ${slack}
 
 cd /home/pi/pictures/timelapse
 ls *.jpg > stills.txt
@@ -9,5 +8,5 @@ mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 
 
 s3cmd mv --acl-public s3://picam-garden-jesse/tl/timelapse-latest.avi  s3://picam-garden-jesse/tl/timelapse-$(date --date="yesterday" +"%Y-%m-%d")
 s3cmd put --acl-public timelapse.avi s3://picam-garden-jesse/tl/timelapse-latest.avi
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Done with timelapse for the day: https://picam-garden-jesse.s3.us-east-2.amazonaws.com/tl/timelapse-latest.avi"}' ${slack}
 
-wall -n Done with timelapse for the day
