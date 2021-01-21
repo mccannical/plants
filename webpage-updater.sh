d4028a8ebf81ed7d165b1f95b90424db480d789c
Jesse <jesse@mccannical.com>
2021-01-20 22:43:05 -0500

    Update webpage-updater.sh

--- webpage-updater.sh
+++ webpage-updater.sh
@@ -16,13 +16,14 @@ wall -n website http://picam-garden-jesse.s3-website.us-east-2.amazonaws.com/
 mkdir timelapse 
 counter=1000
 while true; do
-        wall -n Picture ${counter}. Sleeping ${sleep_time}s
+        wall -n Picture ${counter}. 
         raspistill -w 1024 -h 768 -o raw.jpg
         cp raw.jpg "timelapse/image-${counter}.jpg"
         convert raw.jpg -pointsize 74 -fill white -annotate +100+100 "$(date +"%a %r")"  plants.jpg
         s3cmd put plants.jpg s3://picam-garden-jesse/img/plants.jpg
         s3cmd setacl --acl-public --recursive s3://picam-garden-jesse/img
         rm raw.jpg plants.jpg
+        wall -n Sleeping ${sleep_time}s
         sleep ${sleep_time}
         ((counter=counter+1))
 done
