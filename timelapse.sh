1e48a47bbbf857dc7d624097ac45443917a4e9b4
Jesse <jmccann@confluent.io>
2021-02-09 17:20:55 -0500

    Update timelapse.sh

--- timelapse.sh
+++ timelapse.sh
@@ -2,7 +2,7 @@
 source /home/pi/plants/secrets
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting Processing of Timelapse for the day"}' ${slack}
 
-cd /home/pi/pictures/timelapse
+cd /home/pi/plants/timelapse/
 ls *.jpg > stills.txt
 mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse.avi -mf type=jpeg:fps=24 mf://@stills.txt
 
