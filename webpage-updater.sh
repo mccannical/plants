1c2fefda8bca60295eb541b62535a66ae680f555
Jesse <jmccann@confluent.io>
2021-02-09 13:57:10 -0500

    Update webpage-updater.sh

--- webpage-updater.sh
+++ webpage-updater.sh
@@ -7,7 +7,7 @@ curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting webpa
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Updating index.html: http://picam-garden-jesse.s3-website.us-east-2.amazonaws.com/"}' ${slack}
 s3cmd put --acl-public index.html s3://picam-garden-jesse/index.html
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Let\'s make sure the live stream is high res!"}' ${slack}
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Lets make sure the live stream is high res!"}' ${slack}
 curl 'http://192.168.186.130/control?var=framesize&val=10'   -H 'Connection: keep-alive'   -H 'Pragma: no-cache'   -H 'Cache-Control: no-cache'   -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'   -H 'DNT: 1'   -H 'Accept: */*'   -H 'Referer: http://192.168.186.130/'   -H 'Accept-Language: en-US,en;q=0.9'   --compressed   --insecure
 
 rm -rf timelapse
