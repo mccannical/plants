b31d9a7757b78632caad27cbab1fb02a9098bfe3
Jesse <jmccann@confluent.io>
2021-02-09 16:15:03 -0500

    Update webpage-updater.sh

--- webpage-updater.sh
+++ webpage-updater.sh
@@ -20,8 +20,8 @@ while true; do
         brightness=$(exiftool raw.jpg | grep Brightness | awk '{printf "%d", $4}')
         echo $brightness
         if [ "$brightness" -gt 1 ]; then
-                cp raw.jpg "timelapse/image-${counter}.jpg"
                 convert raw.jpg -pointsize 32 -fill white -annotate +400+180 "$(date +"%a %r")"  plants.jpg
+                cp plants.jpg "timelapse/image-${counter}.jpg"
                 s3cmd put --acl-public plants.jpg s3://picam-garden-jesse/img/plants.jpg
                 rm raw.jpg plants.jpg
         fi
